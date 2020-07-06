2edabab9d46e10ac973dbc177e44f6d5636c12ed
Elizabeth Phillips <hanifahsp@hotmail.com>
2020-07-06 06:07:35 +0200

    Add files via upload
    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["Sample 1", "Sample 2"];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res) {
+	
+var day = date.getDate();
+
+res.render("list", {listTitle: day, newListItems: items});
+	
+});
+
+app.post("/", function(req, res){
+	var item = req.body.newItem;
+	
+	if (req.body.list === "Work List") {
+		workItems.push(item);	
+		res.redirect("/work");	
+		
+	} else {
+	
+	items.push(item);
+	res.redirect("/");
+	
+	}
+	
+});
+
+app.get("/work", function(req, res){
+	res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+	res.render("about");
+});
+	
+//app.post("/work", function(req, res){
+//	var item = req.body.newItem;
+//	workItems.push(item);
+//	res.redirect("/work");
+	
+//});
+	
+app.listen(3000, function(){
+	console.log("Server started on port 3000");
+});
